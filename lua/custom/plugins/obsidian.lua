8a803685224ab2e7e5b11be8efaa751c85cc59f6
nicholas ong <nicholas.ong@bytedance.com>
2025-07-10 12:15:46 +0800

    update: obsidian.nvim

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -18,7 +18,7 @@ return {
 				},
 			},
 			templates = {
-				folder = "~/templates",
+				folder = "~/zettlekasten/templates",
 				date_format = "%Y-%m-%d",
 				time_format = "%H:%M",
 				-- A map for custom variables, the key should be the variable and the value a function
